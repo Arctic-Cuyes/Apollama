fe0b50d6819c5cc048164f6f2e6e712e4be64611
brunomont <brunoadrian.mont@gmail.com>
2023-03-06 09:54:19 -0500

    feat: add provitional service to get posts

--- lib/src/views/home/home_recent.dart
+++ lib/src/views/home/home_recent.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zona_hub/src/components/post/post.dart';
+import 'package:zona_hub/src/services/firabase_service.dart';
 
 class Recientes extends StatefulWidget {
   const Recientes({super.key});
@@ -12,21 +13,24 @@ class _RecientesState extends State<Recientes> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: null,
-      builder: (context, snapshot){
-          return ListView.builder(
-          itemCount: 10, //lista.length
+      future: getPosts(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+          
+        return ListView.builder(
+          itemCount: snapshot.data?.length,
           itemBuilder: (context, index) {
-            // Se reemplazará por la lista de posts obtenida desde la base de datos
-            return const PostComponent(
+            return PostComponent(
+              postText: snapshot.data?[index]['description'],
+              imageUrl: 'https://www.hogarmania.com/archivos/201910/mascota-perdida-XxXx80.jpg',
               userphoto: "https://i.pinimg.com/originals/30/8d/79/308d795c3cac0f8f16610f53df4e1005.jpg",
-              username: "User Name ",
-              imageUrl: "https://www.hogarmania.com/archivos/201910/mascota-perdida-XxXx80.jpg",
-              postText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non massa fringilla, laoreet diam ac, egestas diam. Maecenas ullamcorper nunc eget convallis porttitor. Donec volutpat odio turpis, a interdum velit iaculis ut. Nullam commodo lacinia condimentum. Sed mauris odio, fermentum sit amet odio a, gravida condimentum ipsum. Vestibulum commodo quam ut laoreet blandit. Suspendisse ornare erat nisl, vitae faucibus ante tincidunt in. Aenean rhoncus accumsan ligula, a aliquam turpis gravida vel. Duis diam ligula, rhoncus ullamcorper arcu at, aliquam rutrum felis. Nam semper, orci sed gravida pharetra, enim nisl placerat orci, et tincidunt enim nunc a sapien. Donec eleifend diam vitae elit placerat pulvinar. Sed aliquam tortor sit amet ultrices vulputate.",
+              username: "nobody",
             );
           },
         );
-      } 
+      },
     );
   }
 }
