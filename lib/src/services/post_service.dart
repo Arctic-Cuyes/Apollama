c0f8f07c791082d553683ee9dab47800a61f4dc9
brunomont <brunoadrian.mont@gmail.com>
2023-03-16 13:10:46 -0500

    feat: auth service only to get the current user

--- lib/src/services/post_service.dart
+++ lib/src/services/post_service.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:zona_hub/src/models/post_model.dart';
+import 'package:zona_hub/src/services/Auth/auth_service.dart';
 import 'package:zona_hub/src/services/user_service.dart';
 
 class PostService {
   final CollectionReference postsRef =
       FirebaseFirestore.instance.collection('posts');
   final UserService userService = UserService();
-
+  final AuthService authService = AuthService();
   Stream<QuerySnapshot> streamPosts() {
     final Stream<QuerySnapshot> postsStream = postsRef.snapshots();
     return postsStream;
@@ -16,24 +17,22 @@ class PostService {
     return postsRef.snapshots().asyncMap((snapshot) async {
       final posts = await Future.wait(snapshot.docs.map((doc) async {
         final post = Post.fromJson(doc.data() as Map<String, dynamic>);
-        post.authorData =
-            await userService.getUserDataFromDocRef(post.author!);
+        post.authorData = await userService.getUserDataFromDocRef(post.author!);
+        post.id = doc.id;
+        post.authorData!.id = post.author!.id;
         return post;
       }));
       return posts;
     });
   }
 
-  // Future<void> see() async {
-  //   print("-------------------------------");
-  //   postsRef.snapshots().forEach((snapshot) {
-  //     snapshot.docs
-  //         .map((doc) => Post.fromJson(doc.data() as Map<String, dynamic>))
-  //         .forEach((Post post) async {
-  //       post.authorData =
-  //           await userService.getUserDataFromDocRef(post.author!);
-  //       print(post.authorData!.name);
-  //     });
-  //   });
+  // create a post and set the author to the current user
+  // Future createPost(Post post) async {
+  // }
+
+  // void see() {
+  //   print("____________________________");
+  //   final currentUser = authService.currentUser;
+  //   print(currentUser);
   // }
 }
