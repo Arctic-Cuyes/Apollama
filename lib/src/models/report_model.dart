bf6d60d13819007100f1cfddc5e11f8e061f0246
brunomont <brunoadrian.mont@gmail.com>
2023-03-16 23:55:03 -0500

    refactor: add user field in report model

--- lib/src/models/report_model.dart
+++ lib/src/models/report_model.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:zona_hub/src/models/report_type_model.dart';
 import 'package:zona_hub/src/models/utils/json_document_reference.dart';
+import 'package:zona_hub/src/models/user_model.dart';
 
 class Report {
   Report({
     this.id,
+    required this.user,
     required this.type,
   });
   Report.fromJson(Map<String, dynamic> json)
@@ -12,15 +14,22 @@ class Report {
             id: json['id'] as String? ?? '',
             type: JsonDocumentReference(
               (json['type'] as DocumentReference<Map<String, dynamic>>).path,
-            ).toDocumentReference());
+            ).toDocumentReference(),
+            user: JsonDocumentReference(
+                    (json['user'] as DocumentReference<Map<String, dynamic>>)
+                        .path)
+                .toDocumentReference());
   late String? id;
   final DocumentReference<Map<String, dynamic>> type;
   late ReportType typeData;
+  final DocumentReference<Map<String, dynamic>> user;
+  late User? userData;
 
   Map<String, dynamic> toJson() {
     return {
       if (id != null) 'id': id,
-      'type': type,
+      'type': JsonDocumentReference(type.path).toJson(),
+      'user': JsonDocumentReference(user.path).toJson(),
     };
   }
 }
