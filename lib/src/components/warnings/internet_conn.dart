2fc1d24af7b973ff386daf32a505595b5f85c21c
Hiroshi <ebruno.hiroshi@gmail.com>
2023-03-16 21:30:22 -0500

    feat: connectivity services and connection snackbar added

--- lib/src/components/warnings/internet_conn.dart
+++ lib/src/components/warnings/internet_conn.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class ConnectionWarningCard extends StatelessWidget {
-  const ConnectionWarningCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+SnackBar showConnectionWarningFlash() {
+  return SnackBar(
+    content: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: Icon(
+            Icons.wifi_off,
+            size: 15,
+            color: Colors.white,
+          ),
+        ),
+        Text("Sin conexión a internet",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+            )),
+      ],
+    ),
+    duration: const Duration(
+      minutes: 2,
+    ),
+    behavior: SnackBarBehavior.fixed,
+    backgroundColor: Colors.red.shade500,
+    padding: const EdgeInsets.all(5),
+  );
 }
