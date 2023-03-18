abd363bd2ee55d157ab42d956033b681cc6e0c41
brunomont <brunoadrian.mont@gmail.com>
2023-03-18 16:42:00 -0500

    feat: add queryby method an use it in getPosts method in post service

--- lib/src/services/post_service.dart
+++ lib/src/services/post_service.dart
@@ -1,22 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:zona_hub/src/models/post_model.dart';
+import 'package:zona_hub/src/models/tag_model.dart';
 import 'package:zona_hub/src/services/Auth/auth_service.dart';
+import 'package:zona_hub/src/services/tag_service.dart';
 import 'package:zona_hub/src/services/user_service.dart';
+import 'package:zona_hub/src/utils/post_query.dart';
 
 class PostService {
-  final CollectionReference postsRef =
-      FirebaseFirestore.instance.collection('posts');
+  final postsRef = FirebaseFirestore.instance
+      .collection('posts')
+      .withConverter<Post>(
+          fromFirestore: (snapshots, _) =>
+              Post.fromJson(snapshots.data() as Map<String, dynamic>),
+          toFirestore: (post, _) => post.toJson());
   final UserService userService = UserService();
   final AuthService authService = AuthService();
-  Stream<QuerySnapshot> streamPosts() {
-    final Stream<QuerySnapshot> postsStream = postsRef.snapshots();
-    return postsStream;
-  }
+  final TagService tagService = TagService();
 
-  Stream<List<Post>> getPosts() {
-    return postsRef.snapshots().asyncMap((snapshot) async {
+  Stream<List<Post>> getPosts(
+      {PostQuery query = PostQuery.all, List<Tag> tags = const <Tag>[]}) {
+    checkPostQueryParams(query: query, tags: tags);
+    return postsRef
+        .queryBy(query: query, tags: tags)
+        .snapshots()
+        .asyncMap((snapshot) async {
       final posts = await Future.wait(snapshot.docs.map((doc) async {
-        final post = Post.fromJson(doc.data() as Map<String, dynamic>);
+        final post = doc.data();
         post.authorData = await userService.getUserDataFromDocRef(post.author!);
         post.id = doc.id;
         post.authorData!.id = post.author!.id;
@@ -28,11 +37,5 @@ class PostService {
 
   // create a post and set the author to the current user
   // Future createPost(Post post) async {
-  // }
-
-  // void see() {
-  //   print("____________________________");
-  //   final currentUser = authService.currentUser;
-  //   print(currentUser);
   // }
 }
