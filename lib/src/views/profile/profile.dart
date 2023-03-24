6d75785b33c1fc78e621d6ee3928d243935f0e06
Johann Castillo <johannjco15022@gmail.com>
2023-03-24 16:02:03 -0500

    using NestedScrollView

--- lib/src/views/profile/profile.dart
+++ lib/src/views/profile/profile.dart
@@ -40,8 +40,9 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage> with SingleTickerProviderStateMixin {
   final AuthService authService = AuthService();
+  late TabController _tabBarController;
   bool isMyProfile = false;
 
   @override
@@ -50,6 +51,13 @@ class _ProfilePageState extends State<ProfilePage> {
     super.initState();
     initializeDateFormatting('es');
     checkIsMyProfile();
+    _tabBarController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabBarController.dispose();
+    super.dispose();
   }
 
   checkIsMyProfile() async {
@@ -91,12 +99,6 @@ class _ProfilePageState extends State<ProfilePage> {
             return Scaffold(
                 appBar: AppBar(
                   automaticallyImplyLeading: false,
-                  titleTextStyle: TextStyle(
-                      overflow: TextOverflow.visible,
-                      fontSize: 16,
-                      color: MyApp.themeNotifier.value == ThemeMode.dark
-                          ? Colors.white
-                          : Colors.grey[500]),
                   leading: IconButton(
                     tooltip: "Atrás",
                     onPressed: () {
@@ -120,160 +122,176 @@ class _ProfilePageState extends State<ProfilePage> {
                             icon: const Icon(Icons.info)),
                   ],
                 ),
-                body: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      // CARD DATOS DE USUARIO
-                      Card(
+                body: NestedScrollView(
+                  headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+                // These are the slivers that show up in the "outer" scroll view.
+                  return <Widget>[
+                    
+                      SliverToBoxAdapter(
+                        child: Card(
                         elevation: 0,
                         child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
-                          child: Column(children: [
-                            Flex(
-                                direction: Axis.horizontal,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  //photo, name and email
-                                  Column(
-                                    // crossAxisAlignment: CrossAxisAlignment.ce,
-                                    children: [
-                                      GestureDetector(
-                                        onTap: (){
-                                            isMyProfile ? 
-                                              Storage().uploadProfileImage(ImageSource.gallery) : 
-                                                openProfilePhoto(snapshot.data!.avatarUrl!);
-                                            },
-                                        child: Stack(children: [
-                                          ClipOval(
-                                            child: Image.network(
-                                              snapshot.data!.avatarUrl!,
-                                              width: 100,
-                                              height: 100,
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ),
-                                          isMyProfile
-                                              ? const Positioned(
-                                                  bottom: 0,
-                                                  left: 0,
-                                                  child: Icon(
-                                                    Icons.add_a_photo,
-                                                    size: 20,
-                                                  ))
-                                              : const SizedBox.shrink(),
-                                        ]),
-                                      ),
-
-                                      const SizedBox(
-                                        height: 10,
-                                      ),
-                                      Text(
-                                        snapshot.data!.name,
-                                        style: const TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      //const SizedBox(height: 10,),
-                                      Text(snapshot.data!
-                                          .email), // El usuario decide si mostrar o no el email
-                                    ],
-                                  ),
-                                ]),
-                            //Información extra: fecha de creación de perfil, ubicación,
-                            //  UBICACIÓN
-                            const SizedBox(height: 10,),
-                            ExtraInfo(
-                                icon: const Icon(Icons.my_location),
-                                text: snapshot.data!.location.toString()),
-                            //FECHA DE REGISTRO
-                            ExtraInfo(
-                              icon: const Icon(Icons.waving_hand_outlined),
-                              text:
-                                  "Se unió el ${DateFormat("d 'de' MMMM 'del' yyyy", 'es').format(DateFormat('dd-MM-yyyy hh:mm:ss').parse(snapshot.data!.createdAt!))}",
-                            ),
-                            //NUMERO DE COMUNIDADES
-
-                            Flex(direction: Axis.horizontal, children: const [
-                              ExtraInfo(
-                                  icon: Icon(Icons.public),
-                                  text: "Comunidades 10"),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              ExtraInfo(
-                                  icon: Icon(Icons.post_add),
-                                  text: "Publicaciones 1000")
-                            ]),
-                          ]),
-                        ),
-                      ), //CARD DATOS DE USUARIO ENDS
-                      
-                      //CARD AÑADE UNA PUBLICACIÓN
-                     Offstage(
-                       offstage: !isMyProfile,
-                       child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10,0,0,0),
-                         child: Row(
-                          children: [
-                            FloatingActionButton(
-                              onPressed: (){
-                                //Go to create post page
-                              },
-                              child: const Icon(Icons.edit_square),
-                            ),
-                            const Text("   Crea una nueva publicación")
-                          ],
-                         ),
-                       ),
-                     ),
-
-                      // UPS Y PUBLICACIONES DE PERFIL
-
-                      Card(
-                        child: DefaultTabController(
-                          length: 2,
-                          child: Flex(
-                            direction: Axis.vertical,
-                            children: [
-                              const SizedBox(
-                                height: 40,
-                                child: TabBar(
-                                    indicatorSize: TabBarIndicatorSize.tab,
-                                    tabs: [
-                                      ListTile(
-                                        horizontalTitleGap: 0,
-                                        minLeadingWidth: 20,
-                                        leading: Icon(Icons.fact_check, size: 20,),
-                                        contentPadding: EdgeInsets.all(0), 
-                                        title: Center(child: Text("Mis publicaciones", style: TextStyle(fontSize: 14,),)),
-                                      ),
-                                      ListTile(
-                                        horizontalTitleGap: 0,
-                                        minLeadingWidth: 20,
-                                      
-                                        leading: Icon(Icons.thumb_up, size: 20,), 
-                                        title: Center(child: Text("Me gusta", style: TextStyle(fontSize: 14),)),
-                                      ),
-                                    ]),
-                              ),
-                              //Mostrar TabBarViews
-                              SizedBox(
-                                height: 720, 
-                                child: TabBarView(
+                        padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+                        child: Column(children: [
+                          Flex(
+                              direction: Axis.horizontal,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                //photo, name and email
+                                Column(
+                                  // crossAxisAlignment: CrossAxisAlignment.ce,
                                   children: [
-                                    Posts(authorID: snapshot.data!.id!),
-                                    Posts(authorID: snapshot.data!.id!), // Liked posts
+                                    GestureDetector(
+                                      onTap: (){
+                                          isMyProfile ? 
+                                            Storage().uploadProfileImage(ImageSource.gallery) : 
+                                              openProfilePhoto(snapshot.data!.avatarUrl!);
+                                          },
+                                      child: Stack(children: [
+                                        ClipOval(
+                                          child: Image.network(
+                                            snapshot.data!.avatarUrl!,
+                                            width: 100,
+                                            height: 100,
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        isMyProfile
+                                            ? const Positioned(
+                                                bottom: 0,
+                                                left: 0,
+                                                child: Icon(
+                                                  Icons.add_a_photo,
+                                                  size: 20,
+                                                ))
+                                            : const SizedBox.shrink(),
+                                      ]),
+                                    ),
+
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      snapshot.data!.name,
+                                      style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    //const SizedBox(height: 10,),
+                                    Text(snapshot.data!
+                                        .email), // El usuario decide si mostrar o no el email
                                   ],
                                 ),
-                              )
-                            ],
+                                ]
+                              ),
+                              //Información extra: fecha de creación de perfil, ubicación,
+                              //  UBICACIÓN
+                              const SizedBox(height: 10,),
+                              ExtraInfo(
+                                  icon: const Icon(Icons.my_location),
+                                  text: snapshot.data!.location.toString()),
+                              //FECHA DE REGISTRO
+                              ExtraInfo(
+                                icon: const Icon(Icons.waving_hand_outlined),
+                                text:
+                                    "Se unió el ${DateFormat("d 'de' MMMM 'del' yyyy", 'es').format(DateFormat('dd-MM-yyyy hh:mm:ss').parse(snapshot.data!.createdAt!))}",
+                              ),
+                              //NUMERO DE COMUNIDADES
+
+                              Flex(direction: Axis.horizontal, children: const [
+                                ExtraInfo(
+                                    icon: Icon(Icons.public),
+                                    text: "Comunidades 10"),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                ExtraInfo(
+                                    icon: Icon(Icons.post_add),
+                                    text: "Publicaciones 1000")
+                              ]),
+                            ]),
+                            ),
+                          ),
+                      ),
+                      /// USER DATA CARD
+                      
+                      // ************* Añade una publicación **********
+                      
+                      SliverToBoxAdapter(
+                        child: Offstage(
+                          offstage: !isMyProfile,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(10,0,0,0),
+                            child: Row(
+                              children: [
+                                FloatingActionButton(
+                                  onPressed: (){
+                                    //Go to create post page
+                                  },
+                                  child: const Icon(Icons.edit_square),
+                                ),
+                                const Text("   Crea una nueva publicación")
+                              ],
+                            ),
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                )
-              );
+                      ),
+   
+                      SliverAppBar(
+                        elevation: 0,
+                        toolbarHeight: 0,
+                        pinned: true,
+                        bottom: PreferredSize(
+                          preferredSize: const Size.fromHeight(58),
+                          child: TabBar(
+                              controller: _tabBarController,
+                              indicatorSize: TabBarIndicatorSize.tab,
+                              tabs: const [
+                                ListTile(
+                                  horizontalTitleGap: 0,
+                                  minLeadingWidth: 20,
+                                  leading: Icon(
+                                    Icons.fact_check,
+                                    size: 20,
+                                  ),
+                                  contentPadding: EdgeInsets.all(0),
+                                  title: Center(
+                                      child: Text(
+                                    "Mis publicaciones",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                    ),
+                                  )),
+                                ),
+                                ListTile(
+                                  horizontalTitleGap: 0,
+                                  minLeadingWidth: 20,
+                                  leading: Icon(
+                                    Icons.thumb_up,
+                                    size: 20,
+                                  ),
+                                  title: Center(
+                                      child: Text(
+                                    "Me gusta",
+                                    style: TextStyle(fontSize: 14),
+                                  )),
+                                ),
+                              ]),
+                          ),
+                        ),
+                  ];
+                
+                  },
+                  body: TabBarView(
+                          controller: _tabBarController,
+                          children: [
+                            Posts(authorID: snapshot.data!.id!),
+                            Posts(authorID: snapshot.data!.id!),
+                          ],
+                               
+                        ),
+                ),
+              );       
           }else{
             return const Center(child: CircularProgressIndicator(),);
           }
@@ -323,7 +341,7 @@ class _PostsState extends State<Posts> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Post>>(
-      stream: postService.getPostsByAuthorId(widget.authorID),
+      stream: postService.getPostsByAuthorId(widget.authorID), // Por parámetro
       builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
         if (snapshot.hasError) {
           return const Text('Something went wrong');
