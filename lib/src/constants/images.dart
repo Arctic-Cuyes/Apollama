4a0a72110b1f9b19bb2a4210064b5d0e5791abc1
Hiroshi <ebruno.hiroshi@gmail.com>
2023-03-30 16:52:24 -0500

    feat: new logo added

--- lib/src/constants/images.dart
+++ lib/src/constants/images.dart
@@ -1,10 +1,9 @@
 // import 'package:flutter/material.dart';
 
 class GlobalConstansImages {
-  static const String urlLogo = "assets/images/ZH_Logo.png";
+  static const String urlLogo = "assets/images/apollama.png";
 
   static const String facebookSVG = "assets/icons/facebook_svg.svg";
   static const String googleSVG = "assets/icons/google_svg.svg";
   static const String googleMapMarker = "assets/icons/map_marker_icon.png";
-
 }
